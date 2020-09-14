230c90224a14ffba7427f5d67e09d5b9217a916b
SpiralP <SpiralP@users.noreply.github.com>
2020-09-14 05:42:05 +0000

    deploy: 5f6eb6777c32121106091997ebea0290b793cbce

--- implementors/error_chain/trait.ChainedError.js
+++ implementors/error_chain/trait.ChainedError.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {};
 implementors["error_chain"] = [];
-implementors["invidious"] = [{"text":"impl <a class=\"trait\" href=\"error_chain/trait.ChainedError.html\" title=\"trait error_chain::ChainedError\">ChainedError</a> for <a class=\"struct\" href=\"invidious/struct.Error.html\" title=\"struct invidious::Error\">Error</a>","synthetic":false,"types":["invidious::error::Error"]}];
+implementors["invidious"] = [{"text":"impl ChainedError for Error","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
